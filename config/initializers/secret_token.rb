a84aafb350220d751294c9c827498edd270ed6f8
Thanh Tien <nhoxtien2010@gmail.com>
2018-01-29 09:55:16 +0700

    init application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MongoEav::Application.config.secret_token = '4f7f8dbc9f083ff7320ecf2ebee4f988f86d4537493b5043014a6ec547a1077ce0e6651f9c683aea695cdee8fba507b8ebf763dbda27cf406b9dcce502a45aaf'
